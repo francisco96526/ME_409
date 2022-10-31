7b5b9ecc43539639ebeb54f28d1efbf440da8055
NicholeiSH <hansen.nicholei@gmail.com>
2022-10-31 09:36:13 +0100

    New update

--- extractdata.py
+++ extractdata.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 
 filename = "cost_breakdown.csv"
-filePath = Path("example_output_data",filename)
+filePath = Path("data","example_output_data",filename)
 fileData = pd.read_csv(filePath,sep='\t')
 
 print(fileData)
