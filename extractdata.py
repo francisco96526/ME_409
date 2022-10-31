449a7185fffde87be29f3d85e835f33793c91da3
NicholeiSH <hansen.nicholei@gmail.com>
2022-10-31 19:22:48 +0100

    Update extractdata.py

--- extractdata.py
+++ extractdata.py
@@ -2,8 +2,18 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-filename = "cost_breakdown.csv"
-filePath = Path("data","example_output_data",filename)
-fileData = pd.read_csv(filePath,sep='\t')
+foldername = "example_output_data"
+filenames = ["cost_breakdown.csv","end_use.csv","energy_output.csv","gwp_breakdown.csv","installed_technologies.csv","losses.csv","monthly_operation.csv","performance.csv","shares.csv","storage.csv"]
+filePath = Path("data",foldername,filenames[0])
+df = pd.read_csv(filePath,sep='\t')
 
-print(fileData)
+split1 = df.index[df['Technology'] == 'Storage technology'][0]
+split2 = df.index[df['Technology'] == 'Resource'][0]
+
+table1 = df[0:split1]
+
+print(df[split1:split2])
+print('---')
+print(df[split2:])
+
+
