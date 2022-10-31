33168c1d282fa1323e0fb044f002c53dace38992
NicholeiSH <hansen.nicholei@gmail.com>
2022-10-31 09:17:49 +0100

    New extractdata

---
+++ extractdata.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+filename = "cost_breakdown.csv"
+filePath = Path("example_output_data",filename)
+fileData = pd.read_csv(filePath,sep='\t')
+
+print(fileData)
