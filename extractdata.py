e98cbd461186c29ae01c2b0720e819a867b0598b
NicholeiSH <hansen.nicholei@gmail.com>
2022-11-08 07:51:08 +0100

    Updates, first file done

--- extractdata.py
+++ extractdata.py
@@ -7,13 +7,22 @@ filenames = ["cost_breakdown.csv","end_use.csv","energy_output.csv","gwp_breakdo
 filePath = Path("data",foldername,filenames[0])
 df = pd.read_csv(filePath,sep='\t')
 
-split1 = df.index[df['Technology'] == 'Storage technology'][0]
-split2 = df.index[df['Technology'] == 'Resource'][0]
+split1 = df.index[df['Technology'] == 'Storage technology'][0] + 1
+split2 = df.index[df['Technology'] == 'Resource'][0] + 1
 
 table1 = df[0:split1]
 
-print(df[split1:split2])
-print('---')
-print(df[split2:])
+table2 = df[split1:split2-1]
+table3 = df[split2:]
 
+table2 = table2.rename(columns={'Technology':'Storage technology'})
+table3 = table3.rename(columns={'Technology':'Resource','C_inv [MCHF/y]':'C_op [MCHF/y]'})
+table3 = table3[['Resource','C_op [MCHF/y]']]
 
+filePath1 = Path("data",foldername,"Extracted_cost_breakdown_1.csv")
+filePath2 = Path("data",foldername,"Extracted_cost_breakdown_2.csv")
+filePath3 = Path("data",foldername,"Extracted_cost_breakdown_3.csv")
+
+table1.to_csv(filePath1)
+table2.to_csv(filePath2)
+table3.to_csv(filePath3)
